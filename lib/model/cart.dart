802db4f00f5e794f8a7d39ecfda5620a93278341
Mathis <eyzex@hotmail.fr>
2023-01-23 17:52:11 +0100

    fix(card): fixed prices bug

--- lib/model/cart.dart
+++ lib/model/cart.dart
@@ -27,16 +27,38 @@ class cardModel {
 
   factory cardModel.fromJson(Map<String, dynamic> json) {
     List<CardSet> cardSets = [];
+    Map<CardSeller, double> prices = {};
     if (json['card_sets'] != null) {
       for (var cardSet in json['card_sets']) {
         print(cardSet['set_price']);
         cardSets.add(CardSet(
             name: cardSet['set_name'],
             code: cardSet['set_code'],
-            price: cardSet['set_price'] != null && cardSet['set_price'].runtimeType == "double"
+            price: cardSet['set_price'] != null &&
+                    cardSet['set_price'].runtimeType == "double"
                 ? double.parse(cardSet['set_price'])
                 : 0));
       }
+    }
+    if (json['card_prices'][0]['amazon_price'] != null) {
+      prices[CardSeller.amazonPrice] =
+          double.parse(json['card_prices'][0]['amazon_price']);
+    }
+    if (json['card_prices'][0]['cardmarket_price'] != null) {
+      prices[CardSeller.cardmarketPrice] =
+          double.parse(json['card_prices'][0]['cardmarket_price']);
+    }
+    if (json['card_prices'][0]['coolstuffinc_price'] != null) {
+      prices[CardSeller.coolstuffincPrice] =
+          double.parse(json['card_prices'][0]['coolstuffinc_price']);
+    }
+    if (json['card_prices'][0]['ebay_price'] != null) {
+      prices[CardSeller.ebayPrice] =
+          double.parse(json['card_prices'][0]['ebay_price']);
+    }
+    if (json['card_prices'][0]['tcgplayer_price'] != null) {
+      prices[CardSeller.tcgplayerPrice] =
+          double.parse(json['card_prices'][0]['tcgplayer_price']);
     }
 
     return cardModel(
@@ -47,18 +69,7 @@ class cardModel {
         race: json['race'],
         url: json['card_images'][0]['image_url'],
         sets: cardSets,
-        prices: {
-          CardSeller.amazonPrice:
-              double.parse(json['card_prices'][0]['amazon_price']),
-          CardSeller.cardmarketPrice:
-              double.parse(json['card_prices'][0]['cardmarket_price']),
-          CardSeller.coolstuffincPrice:
-              double.parse(json['card_prices'][0]['coolstuffinc_price']),
-          CardSeller.ebayPrice:
-              double.parse(json['card_prices'][0]['ebay_price']),
-          CardSeller.tcgplayerPrice:
-              double.parse(json['card_prices'][0]['tcgplayer_price'])
-        },
+        prices: prices,
         attribute: json['attribute'],
         level: json['level']);
   }
